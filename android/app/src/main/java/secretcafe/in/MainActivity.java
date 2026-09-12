6fab11f9c0491ece5a887859a8bbe3544ec2c8da
rushikesh1629 <rushikeshbhor1629@gmail.com>
2026-09-12 19:28:36 +0530

    fix

--- android/app/src/main/java/secretcafe/in/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package secretcafe.in;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'secretcafe.in',
-  appName: 'Secret Cafe POS',
-  webDir: 'public'
-};
-
-export default config;
