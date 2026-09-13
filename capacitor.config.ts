562298e34baa12297b5a83cd02871e648b8b3711
rushikesh1629 <rushikeshbhor1629@gmail.com>
2026-09-13 19:07:23 +0530

    fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: '4secretcafe',
+  appName: 'secretcafe',
   webDir: 'out',
   server: {
     androidScheme: 'https',
