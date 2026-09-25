dfa7879f1dc2a100feddd28bbee0ebdfed4092ec
rushikesh1629 <rushikeshbhor1629@gmail.com>
2026-09-25 19:43:50 +0530

    fix package name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.secretcafe.pos',
   appName: 'secretcafe',
   webDir: 'out',
   server: {
